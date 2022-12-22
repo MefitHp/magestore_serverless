459309160b91bea3fca8986e1d846bedebc29b97
Mefit Hernández <mefitdev@gmail.com>
2022-12-21 21:29:01 -0600

    Added mock data

---
+++ mockData.ts
@@ -0,0 +1,36 @@
+import { Candy } from "./types";
+
+export const candies: Candy[] = [
+  {
+    id: "any-id-1",
+    name: "Churrumais",
+    unitPrice: 10,
+    imageURL:
+      "https://res.cloudinary.com/walmart-labs/image/upload/d_default.jpg/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750047800255L.jpg",
+    hasStock: true,
+  },
+  {
+    id: "any-id-2",
+    name: "Rockaleta",
+    unitPrice: 5,
+    imageURL:
+      "https://res.cloudinary.com/walmart-labs/image/upload/d_default.jpg/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750101110545L3.jpg?odnHeight=80&odnWidth=80&odnBg=FFFFFF",
+    hasStock: true,
+  },
+  {
+    id: "any-id-3",
+    name: "Top Tops Morados",
+    unitPrice: 12,
+    imageURL:
+      "http://cdn.shopify.com/s/files/1/2164/2083/products/7500366001831_800x.png?v=1552613122",
+    hasStock: true,
+  },
+  {
+    id: "any-id-4",
+    name: "Top Tops Morados",
+    unitPrice: 12,
+    imageURL:
+      "https://res.cloudinary.com/walmart-labs/image/upload/d_default.jpg/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750036600067L.jpg",
+    hasStock: true,
+  },
+];
